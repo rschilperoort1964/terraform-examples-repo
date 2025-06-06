36d961a281af772ed3c8d777f039fa6aa7201fc7
Hidde de Smet <desmet.hidde@hotmail.com>
2025-06-06 12:13:31 +0200

    Examples and lab added to the repo

---
+++ Module-1/Lab/main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-${var.project}"
+  location = var.location
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
